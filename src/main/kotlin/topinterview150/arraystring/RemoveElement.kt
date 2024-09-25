ef8f19ea3e090807283f44a9f5c63ae7c1c59f27
Israel Mendoza <artisra@amazon.com>
2024-09-25 00:02:14 -0600

    27. Remove Element - Refactored, better solution [KOTLIN]

--- src/main/kotlin/topinterview150/arraystring/RemoveElement.kt
+++ src/main/kotlin/topinterview150/arraystring/RemoveElement.kt
@@ -3,39 +3,15 @@ package dev.artisra.topinterview150.arraystring
 // https://leetcode.com/problems/remove-element/description/
 
 class RemoveElement {
-    fun removeElement(nums: IntArray, value: Int): Int {
+    fun removeElement(numbers: IntArray, value: Int): Int {
+        var availableIndex = 0
 
-        if (nums.isEmpty()) return 0 // There is nothing to remove from
-
-        // Creating variables for pointers and remove count:
-        var leftPointer = 0
-        var rightPointer = nums.size
-        var removeCount = 0
-
-        // "Removing" from the end of the array
-        while (true) {
-            rightPointer--
-            if (rightPointer < 0 || nums[rightPointer] != value) break
-            removeCount++
-        }
-
-        // Moving the pointers towards each other:
-        while (leftPointer <= rightPointer) {
-            if (nums[leftPointer] == value) { // We've found a number to remove
-                // Storing a valid number in the place of a value to remove:
-                nums[leftPointer] = nums[rightPointer] // Right pointer will always be different from the value
-                removeCount++
-
-                // Moving the right pointer to a number different from the value
-                while (true) {
-                    rightPointer--
-                    if (rightPointer < 0 || nums[rightPointer] != value) break
-                    removeCount++
-                }
+        for (number in numbers) {
+            if (number != value) {
+                numbers[availableIndex] = number
+                availableIndex++
             }
-            leftPointer++
         }
-
-        return nums.size - removeCount
+        return availableIndex
     }
 }
