fffb43236c50ec5707d130c834a7de6343f7de02
Israel Mendoza <artisra@amazon.com>
2024-10-11 14:21:58 -0600

    1431. Kids With the Greatest Number of Candies [KOTLIN]

---
+++ src/main/kotlin/topinterview150/arraystring/KidsWithTheGreatestNumberOfCandies.kt
@@ -0,0 +1,22 @@
+package dev.artisra.topinterview150.arraystring
+
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/solutions/5900825/using-map-time-o-n-space-o-n-kotlin/
+
+class KidsWithTheGreatestNumberOfCandies {
+    fun kidsWithCandiesProcedural(candies: IntArray, extraCandies: Int): List<Boolean> {
+        val result = mutableListOf<Boolean>()
+        val max = candies.max()
+
+        for (candy in candies) {
+            val totalCandies = candy + extraCandies
+            result.add(totalCandies >= max)
+        }
+        return result
+    }
+
+    fun kidsWithCandiesFunctional(candies: IntArray, extraCandies: Int): List<Boolean> {
+        val max = candies.max()
+        return candies.map { it + extraCandies >= max }
+    }
+}
