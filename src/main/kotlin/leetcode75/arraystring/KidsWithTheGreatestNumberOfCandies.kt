5439c52701c8a18eb1a2b1f9750ccc46f11472f7
Israel Mendoza <artisra@amazon.com>
2024-10-13 23:23:14 -0600

    Created new package for LeetCode75. Moved one file there

--- src/main/kotlin/leetcode75/arraystring/KidsWithTheGreatestNumberOfCandies.kt
+++ src/main/kotlin/leetcode75/arraystring/KidsWithTheGreatestNumberOfCandies.kt
@@ -1,4 +1,4 @@
-package dev.artisra.topinterview150.arraystring
+package dev.artisra.leetcode75.arraystring
 
 // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/
 // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/solutions/5900825/using-map-time-o-n-space-o-n-kotlin/
